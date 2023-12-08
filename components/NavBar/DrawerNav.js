38eb9b5fe424d7e29a7cecfb655c3d4b30a18b79
Khaled Hazem <khaled.hazem2009@gmail.com>
2023-12-08 04:41:28 +0300

    Adding navbar

---
+++ components/NavBar/DrawerNav.js
@@ -0,0 +1,59 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { AiOutlineClose, AiOutlineMail } from "react-icons/ai";
+import { FaGithub, FaLinkedinIn } from "react-icons/fa";
+import { BsFillPersonLinesFill } from "react-icons/bs";
+
+function DrawerNav(props) {
+  const { nav, handleNav } = props;
+
+  return (
+    <div
+      className={
+        nav ? "md:hidden fixed right-0 top-0 w-full h-full bg-black/40 " : ""
+      }
+    >
+      <div
+        className={
+          nav
+            ? "fixed right-0 top-0 w-[80%] sm:w-[60%] md:w-[45%] h-full backdrop-blur-sm bg-black/40 p-10 ease-in duration-500"
+            : "fixed right-[-100%] w-[75%] top-0 h-full p-10 ease-in duration-500"
+        }
+      >
+        <div>
+          <div className="flex w-full items-center pt-4 justify-between">
+            <div />
+            <div
+              onClick={handleNav}
+              className="rounded-[15px] bg-gray-700 text-white shadow-gray-400 p-3 cursor-pointer"
+            >
+              <AiOutlineClose />
+            </div>
+          </div>
+          <div>
+            <ul className="py-4  pt-20 font-inter text-right text-base font-base leading-norma flex-col">
+              <Link href="">
+                <li onClick={handleNav} className="py-4 text-[18px] text-bold ">
+                  Home
+                </li>
+              </Link>
+              <Link href="/#about">
+                <li onClick={handleNav} className="py-4 text-[18px] text-bold ">
+                  Product
+                </li>
+              </Link>
+              <Link href="/#skills">
+                <li onClick={handleNav} className="py-4 text-[18px] text-bold ">
+                  FAQ
+                </li>
+              </Link>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default DrawerNav;
---
+++ components/NavBar/Navbar.js
@@ -0,0 +1,85 @@
+import Image from "next/image";
+import Link from "next/link";
+import { AiOutlineMenu } from "react-icons/ai";
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedinIn, FaRegComment } from "react-icons/fa6";
+import { TiHomeOutline } from "react-icons/ti";
+import { LiaProjectDiagramSolid } from "react-icons/lia";
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import DrawerNav from "./DrawerNav";
+
+const Navbar = ({ isLight }) => {
+  const [nav, setNav] = useState(false);
+  const router = useRouter();
+
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
+  return (
+    <div className="fixed w-full h-20 z-[100] duration-200">
+      <div className=" w-full h-full flex justify-center pt-4 px-1 2xl:px-16">
+        <div className=" w-[90%] md:max-w-[1300px] h-[100%] border border-gray-700 backdrop-blur-sm bg-white/10 rounded-xl pl-5 flex justify-between lg:justify-around items-center ">
+          <div className=" md:pl-0 ">
+            <h1 className=" text-indigo-500 pb-1 text-[40px] font-semibold ">
+              Khaled
+            </h1>
+          </div>
+
+          <div className=" flex">
+            <ul className=" pr-5 hidden lg:flex">
+              <Link href="/">
+                <li className="ml-10 font-normal font-circularstd tracking-tight transition-all duration-100 flex items-center ">
+                  <div className=" pr-1 ">
+                    <TiHomeOutline />
+                  </div>
+                  Home
+                </li>
+              </Link>
+              <Link href="/#prod">
+                <li className="ml-10 font-normal font-circularstd tracking-tight transition-all duration-100 flex items-center ">
+                  <div className=" pr-1 ">
+                    <LiaProjectDiagramSolid className=" scale-110 " />
+                  </div>
+                  Projects
+                </li>
+              </Link>
+              <Link href="/#faq">
+                <li className="ml-10 font-normal font-circularstd tracking-tight transition-all duration-100 flex items-center ">
+                  <div className=" pr-1 ">
+                    <FaRegComment />
+                  </div>
+                  FAQ
+                </li>
+              </Link>
+            </ul>
+            <div onClick={handleNav} className=" text-[#7C3AED] pr-5 lg:hidden">
+              <AiOutlineMenu size={25} />
+            </div>
+          </div>
+
+          <div className=" hidden lg:flex gap-3 ">
+            <a
+              href="https://github.com/KhaledHN"
+              className=" w-8 h-8 bg-white/30 rounded-full flex justify-center items-center "
+            >
+              <FaGithub className=" scale-110 " />
+            </a>
+            <a
+              href=""
+              className=" w-8 h-8 bg-white/30 rounded-full flex justify-center items-center "
+            >
+              <FaLinkedinIn />
+            </a>
+          </div>
+        </div>
+      </div>
+
+      {/* drawer menu */}
+      <DrawerNav nav={nav} handleNav={handleNav} />
+    </div>
+  );
+};
+
+export default Navbar;
