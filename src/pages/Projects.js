a8f4b5454083cfb58da85b94fd4a5c66a2296d4c
Khaled Hazem <khaled.hazem2009@gmail.com>
2023-12-12 23:06:37 +0300

    fixing responsive design

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -18,8 +18,8 @@ const Projects = () => {
           <p className=" pt-2 text-gray-300 tracking-tight text-base transition-all duration-100 ">
             Some completed projects and continue to work on new ones.
           </p>
-          <div className=" w-full h-[1870px] xl:h-96 flex items-center justify-center xl:justify-normal overflow-x-scroll scrollbar-hide pt-10 xl:pt-0 ">
-            <div className=" xl:flex col-span-8 gap-6 scale-125 xl:scale-100 ">
+          <div className=" w-full h-[1600px] xl:h-96 flex items-center justify-center xl:justify-normal overflow-x-scroll scrollbar-hide pt-10 xl:pt-0 ">
+            <div className=" xl:flex col-span-8 gap-6 scale-110 xl:scale-100 ">
               <div className=" pb-12 xl:pb-0 ">
                 <div className=" w-64 h-[300px] p-4 bg-zinc-900 hover:bg-zinc-800 rounded-lg ">
                   <a
